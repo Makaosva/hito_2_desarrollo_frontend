e055a977b871fea0b2de7b448477306ab7510199
Makaosva <maka.osses@gmail.com>
2024-11-20 21:00:08 -0300

    feat/cambios en el Navbar

--- src/components/NavbarMarket.jsx
+++ src/components/NavbarMarket.jsx
@@ -1,15 +1,29 @@
-import { Navbar } from "react-bootstrap";
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
 function NavbarMarket() {
   const setActiveClass = ({ isActive }) => (isActive ? "active" : undefined);
 
   return (
     <Navbar bg="secondary" variant="dark" expand="lg">
-        <Navbar.Brand>
-            {" "}
-            <img className="Logo" src="" alt=""/>{" "}
-        </Navbar.Brand>
-        
+      <Navbar.Brand>
+        {" "}
+        <img className="Logo" src="" alt="" />{" "}
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic_avbar_nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav className="mr-auto">
+          <Nav.Link as={NavLink} to="/registro" className={setActiveClass}>
+            Register
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/login" className={setActiveClass}>
+            Login
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
+
+export default NavbarMarket;
--- src/context/UsuarioContext.jsx
+++ src/context/UsuarioContext.jsx
@@ -1,8 +1,17 @@
-import { createContext } from "react";
-
+import { createContext, useEffect, useState } from "react";
 
 export const UsuarioContext = createContext();
 
-const UsuariosProvider = ({}) =>{
-    
-}
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+const initialStateToken = localStorage.getItem("token") || null;
+
+const UsuariosProvider = ({ children }) => {
+  const [token, setToken] = useState(initialStateToken);
+
+  useEffect(()=>{
+    if(token){
+        localStorage.setItem("token",)
+    }
+  })
+};
