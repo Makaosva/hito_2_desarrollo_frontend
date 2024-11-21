38617956ced2a3dd0891aceaed1880ac4dc8de00
Carolina2024 <carcastillos@gmail.com>
2024-11-21 13:15:32 -0300

    vista perfil

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import InicioSesion from "./views/InicioSesion";
+import Perfil from "./views/Perfil";
 import NavbarMarket from "./components/NavBarMarket";
 import Registro from "./views/Registro";
 
@@ -9,11 +10,13 @@ function App() {
   return (
     <div>
       <NavbarMarket />
-      <div>Productos
+      <div>
+        Productos
         <Routes>
           {/* <Route path="/" element={<Home />} /> */}
           <Route path="/login" element={<InicioSesion />} />
-          <Route path="/registro" element={<Registro/>} />
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/perfil" element={<Perfil />} />
         </Routes>
       </div>
     </div>
--- src/views/InicioSesion.jsx
+++ src/views/InicioSesion.jsx
@@ -1,5 +1,27 @@
-const InicioSesion = () => {
+/* const InicioSesion = () => {
   return <div>InicioSesion</div>;
+};
+
+export default InicioSesion; */
+
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Container } from "react-bootstrap";
+
+const InicioSesion = () => {
+  const navigate = useNavigate();
+
+  const handleGoToProfile = () => {
+    navigate("/perfil");
+  };
+
+  return (
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <Button variant="primary" onClick={handleGoToProfile}>
+        Ir a Mi Perfil
+      </Button>
+    </Container>
+  );
 };
 
 export default InicioSesion;
