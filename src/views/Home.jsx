7c35e9e46b9d619cde5a79ff42f6cd0f6c0f72a0
Makaosva <maka.osses@gmail.com>
2024-11-23 13:52:40 -0300

    feat/cambios

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,6 +4,7 @@ import OrdenarPor from "../components/OrdenarPor";
 import Buscador from "../components/Buscador";
 import CardPublicacion from "../components/CardPublicacion";
 import { UsuarioContext } from "../context/UsuarioContext";
+import Tienda from "./Tienda";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
@@ -11,8 +12,13 @@ const Home = () => {
   const { publicaciones } = useContext(UsuarioContext);
 
   return (
-    <Container className="container">
-      <h2>Bienvenidos al MarketPlace de cursos en linea de programacion</h2>
+    <Container
+      className="container d-flex flex-column"
+      style={{ minHeight: "100vh" }}
+    >
+      <h2 className="text-center mb-4 p-4">
+        Bienvenidos al MarketPlace de cursos en linea de programacion
+      </h2>
       <Row>
         <Col sm="4">
           <OrdenarPor />
@@ -24,24 +30,10 @@ const Home = () => {
           <Buscador />
         </Col>
       </Row>
-      <Row>
-        {publicaciones.length > 0 ? (
-          publicaciones.map((pub, index) => (
-            <Col xs={12} md={6} key={index}>
-              <CardPublicacion
-                imagen={pub.imagen}
-                titulo={pub.titulo}
-                descripcion={pub.descripcion}
-                precio={pub.precio}
-                publicadoPor={pub.publicadoPor}
-                mostrarAgregar={false} // para ocultar el boton agregar
-              />
-            </Col>
-          ))
-        ) : (
-          <p>No hay publicaciones disponibles</p>
-        )}
-      </Row>
+      
+      <section className="mt-4">
+        <Tienda />
+      </section>
     </Container>
   );
 };
